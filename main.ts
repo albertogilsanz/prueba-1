6e3871e77a9d304b290a14fb0c03648262ee37ff
albertogilsanz <77692577+albertogilsanz@users.noreply.github.com>
2023-11-10 19:55:37 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+    basic.pause(1000)
+    basic.clearScreen()
+})
